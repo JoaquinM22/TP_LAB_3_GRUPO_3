747b827c19758d51ed61463b29826e7f4256bfdd
JoaquinM22 <albarracin.joaquin22@gmail.com>
2023-06-15 10:12:01 -0300

    Primer Commit del Trabajo

---
+++ tpLab3/src/Main.java
@@ -0,0 +1,8 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+        System.out.println("Este es el TP Final de Lab 3");
+    }
+}
