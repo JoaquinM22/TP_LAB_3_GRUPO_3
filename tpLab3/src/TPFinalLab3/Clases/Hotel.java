85ba1d15a4f58b61b9eb26c9cd8221d49acabf65
agus3789 <appollonioagustin@gmail.com>
2023-06-15 21:32:27 -0300

    Creacion de Hotel y ColeccionGenerica cada una con sus atributos y metodos.(FALTA decidir que lista usar en ColeccionGenerica)

---
+++ tpLab3/src/TPFinalLab3/Clases/Hotel.java
@@ -0,0 +1,43 @@
+package TPFinalLab3.Clases;
+
+import java.io.Serializable;
+
+public class Hotel implements Serializable
+{
+    /** ATRIBUTOS **/
+    private static final long serialVersionUID = 2885413278418867930L;
+    String nombre_hotel;
+    ColeccionGenerica<Habitacion> listaHabitaciones = new ColeccionGenerica<>();
+    ColeccionGenerica<Cliente> listaClientes = new ColeccionGenerica<>();
+    ColeccionGenerica<Empleado> listaEmpleados = new ColeccionGenerica<>();
+    ColeccionGenerica<Registro> listaRegistro = new ColeccionGenerica<>();
+
+    /** CONSTRUCTOR **/
+    public Hotel(String nombreHotel) {
+        this.nombreHotel = nombreHotel;
+    }
+
+    /** GETTERS **/
+    public String getNombreHotel() {
+        return nombreHotel;
+    }
+
+    /** GETTERS **/
+    public ColeccionGenerica<Habitacion> getListaHabitaciones() {return listaHabitaciones;}
+    public ColeccionGenerica<Cliente> getListaClientes() {return listaClientes;}
+    public ColeccionGenerica<Empleado> getListaEmpleados() {return listaEmpleados;}
+    public ColeccionGenerica<Registro> getListaRegistro() {return listaRegistro;}
+
+    /** SETTERS **/
+    public void setNombreHotel(String nombreHotel) {this.nombreHotel = nombreHotel;}
+    public void setListaHabitaciones(ColeccionGenerica<Habitacion> listaHabitaciones) {this.listaHabitaciones = listaHabitaciones;}
+    public void setListaClientes(ColeccionGenerica<Cliente> listaClientes) {this.listaClientes = listaClientes;}
+    public void setListaEmpleados(ColeccionGenerica<Empleado> listaEmpleados) {this.listaEmpleados = listaEmpleados;}
+    public void setListaRegistro(ColeccionGenerica<Registro> listaRegistro) {this.listaRegistro = listaRegistro;}
+
+    /** METODOS **/
+    public void mostrarEmpleados(){listaEmpleados.listar();}
+    public void mostrarClientes(){listaClientes.listar();}
+    public void mostrarHabitaciones(){listaHabitaciones.listar();}
+    public void mostrarRegistro(){listaRegistro.listar();}
+}
