7e78c37dd661792729952c91c61eff7373cf9cc5
agus3789 <appollonioagustin@gmail.com>
2023-06-16 16:50:05 -0300

    Agregue funcion para obtener el tamanio de la listaGenerica(Al hacerlo en ColeccionGenerica funciona con todas las listas que se creen a partir de esta).

--- tpLab3/src/TPFinalLab3/Clases/ColeccionGenerica.java
+++ tpLab3/src/TPFinalLab3/Clases/ColeccionGenerica.java
@@ -62,6 +62,10 @@ public class ColeccionGenerica <T> implements Serializable, Iterable<T>
         }
     }
 
+    public int tamanio()
+    {
+        return listaGenerica.size();
+    }
 
     @Override
     public Iterator<T> iterator() {
